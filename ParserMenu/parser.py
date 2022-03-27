e08ebb92c27d5c4bf0ac9ea9ac5fb511ace97cb7
Nikita535 <68530909+Nikita535@users.noreply.github.com>
2022-03-27 20:36:11 +0300

    ebaniy parser

--- ParserMenu/parser.py
+++ ParserMenu/parser.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import lxml
 from bs4 import BeautifulSoup
@@ -14,6 +16,8 @@ def get_first_menu_item():
 
     # print(catalog_list[0])
 
+    cake_dict = {}
+
     for card in catalog_list:
         cake = card.find_all("li", class_="catalog-list__item")
         for c in cake:
@@ -21,8 +25,28 @@ def get_first_menu_item():
             card_desc = c.find("div", class_="c-card-catalog-3__weight").text
             card_price = c.find("div", class_="").text[12:-1]
             card_link_photo_ = str(c.find("div", class_="c-card-catalog-3__img"))
+
             card_link_photo = card_link_photo_[card_link_photo_.find('(')+1:card_link_photo_.find(')')]
+
+            card_link_photo_id = card_link_photo.split("/")[-1]
+            card_link_photo_id = card_link_photo_id[:-4]
+
+            cake_dict[card_link_photo_id] = {
+                "name": card_name,
+                "desc": card_desc,
+                "price": card_price,
+                "card_link_photo": card_link_photo
+            }
 
             print(f"{card_name} | {card_desc} | {card_price} | {card_link_photo}")
 
-get_first_menu_item()
+
+    with open("cake_dict.json", "w", encoding='cp1251') as file:
+        json.dump(cake_dict, file, indent=4,ensure_ascii=True)
+
+def main():
+    get_first_menu_item()
+
+
+if __name__== '__main__':
+    main()
